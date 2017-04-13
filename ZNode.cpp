1b4dfe8f30c0d3fdb82bd43beeac2f04a31cd64e
Kevin <Kevin@DESKTOP-J1KBRTK.T-mobile.com>
2017-04-12 20:56:39 -0400

    Initial commit

---
+++ ZNode.cpp
@@ -0,0 +1,19 @@
+#ifndef ZNODE_HPP_
+#define ZNODE_HPP_
+#include "Z_Obj.hpp"
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+class ZNode {
+    friend class AVLTree;
+    Z_Obj *zip;
+    int height; // max(left->height,right->height) + 1
+    ZNode *left;
+    ZNode *right;
+    ZNode *parent;
+public:
+    ZNode(Z_Obj *z);
+    ZNode();
+};
+#endif /* ZNODE_HPP_ */
---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Kevin
+ *
+ * Created on April 12, 2017, 8:34 PM
+ */
+
+#include <stdlib.h>
+#include <string>
+#include "ZDatabase.hpp";
+using namespace std;
+
+int main() {
+    //string g = "zipcodes.txt"; The full file – after you have everything
+    // working
+    string h = "ziptestfile.txt"; // for testing purposes
+    ZDatabase *zdb = new ZDatabase(h, false);
+    ZDatabase *zdb2 = new ZDatabase(h, true);
+    return 0;
+}
